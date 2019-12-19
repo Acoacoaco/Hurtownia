67345a9942a5d4798098afb02561b7e0fb852d45
Aleksander Wiażewicz <wiazewicz@gmail.com>
2019-12-20 00:31:10 +0100

    first

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+
+// console.log('hi');
+
+// document.getElementsByClassName('progress-bar').style.backgroundColor = 'red';
+// document.getElementsByClassName('progress-bar').style.backgroundColor = 'yellow';
