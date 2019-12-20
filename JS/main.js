902e9f958c8f63f3c18575d0dc2f699eb6e6ba60
Aleksander Wiażewicz <wiazewicz@gmail.com>
2019-12-20 12:48:51 +0100

    v1

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,65 @@
+// varaibles
+const special = document.getElementById('special');
+const news = document.getElementById('news');
+const promo = document.getElementById('promo');
+const military = document.getElementById('military');
+const toys = document.getElementById('toys');
+const gadgets = document.getElementById('gadgets');
+const form = document.getElementById('form');
+           
+// function onload
+function hideAllExceptSpecial() {
+    special.hidden = false;
+    news.hidden = true;
+    promo.hidden = true;
+    military.hidden = true;
+    toys.hidden = true;
+    gadgets.hidden = true;
+    form.hidden = true;
+}
 
-// console.log('hi');
 
-// document.getElementsByClassName('progress-bar').style.backgroundColor = 'red';
-// document.getElementsByClassName('progress-bar').style.backgroundColor = 'yellow';
+// JS functions
+function hideAll() {
+    hideAllExceptSpecial();
+    special.hidden = true;
+}
+
+// onclick functions
+function showNew() {
+    hideAll();
+    news.hidden = false;
+}
+
+function showPromo() {
+    hideAll();
+    promo.hidden = false;
+}
+
+function showProductsAll() {
+    hideAll();
+    military.hidden = false;
+    toys.hidden = false;
+    gadgets.hidden = false;
+}
+
+function showMilitary() {
+    hideAll();
+    military.hidden = false;
+}
+
+function showToys() {
+    hideAll();
+    toys.hidden = false;
+}
+
+function showGadgets() {
+    hideAll();
+    gadgets.hidden = false;
+}
+
+function showForm() {
+    hideAll();
+    form.hidden = false;
+    
+}
