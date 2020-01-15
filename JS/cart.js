95bbcbac0917f35f164a9465a53c34d9a7bc606d
Aleksander Wiażewicz <wiazewicz@gmail.com>
2020-01-15 13:42:28 +0100

    prize as num

--- JS/cart.js
+++ JS/cart.js
@@ -3,6 +3,7 @@ function showCart() {
     mainTitle.innerHTML = 'Twoje zamówienie:';
     formSubpage.style.display = "none";
     cartTable.style.display = "";
+    cartCalculate();
     sideMenuHide();
     window.scrollTo(0,0);
 }
@@ -16,14 +17,14 @@ function addToCart() {
     // alert('Dodano produkt do koszyka.');
     const $imgSrc = $(this).prevAll('img')[0].src;
     const $name = $(this).prevAll('h6')[0].innerText;
-    const $prise = $(this).prevAll('h5')[0].innerText;
-    
-    $('#main-table').prepend('<tr><td><img src="'+$imgSrc+'"<br/><br/><input class="qty" type="number" value="1" aria-label="qty"></input> szt.</td><td><span class="added-name">'+$name+'</span><br/>'+$prise+'</td></tr>')
+    const $h5 = $(this).prevAll('h5');
+    const $prize = $h5.find('.product-prize')[0].innerText;
+
+    $('#main-table').prepend('<tr><td><img src="'+$imgSrc+'"<br/><br/><input class="qty" type="number" value="1" aria-label="qty"></input> szt.</td><td><span class="added-name">'+$name+'</span><br/>'+$prize+' zł</td></tr>');
 }
 
-const $reccalculateBtn = $('#reccalculateBtn');
-$reccalculateBtn.on('click', calculateCart);
-function calculateCart() {
+// calculate cart
+function cartCalculate() {
     
 }
 
