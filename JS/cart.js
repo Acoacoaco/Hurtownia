62588bb414b1f2bd5e2be0a95ecab293f4504670
Aleksander Wiażewicz <wiazewicz@gmail.com>
2020-01-16 11:43:16 +0100

    cart calculator

--- JS/cart.js
+++ JS/cart.js
@@ -3,7 +3,6 @@ function showCart() {
     mainTitle.innerHTML = 'Twoje zamówienie:';
     formSubpage.style.display = "none";
     cartTable.style.display = "";
-    cartCalculate();
     sideMenuHide();
     window.scrollTo(0,0);
 }
@@ -11,22 +10,29 @@ function showCart() {
 // jQuery
 
 const $addToCartBtn = $('.add-button');
-
 $addToCartBtn.on('click', addToCart);
+
 function addToCart() {
     // alert('Dodano produkt do koszyka.');
     const $imgSrc = $(this).prevAll('img')[0].src;
     const $name = $(this).prevAll('h6')[0].innerText;
     const $h5 = $(this).prevAll('h5');
-    const $prize = $h5.find('.product-prize')[0].innerText;
+    var $prize = $h5.find('.product-prize')[0].innerText;
 
-    $('#main-table').prepend('<tr><td><img src="'+$imgSrc+'"<br/><br/><input class="qty" type="number" value="1" aria-label="qty"></input> szt.</td><td><span class="added-name">'+$name+'</span><br/>'+$prize+' zł</td></tr>');
-}
+    $('#main-table').prepend('<tr><td><img src="'+$imgSrc+'"<br/><br/><input class="qty" type="number" value="1" aria-label="qty"></input> szt.</td><td><span class="added-name">'+$name+'</span><br/><span class="in-table-prize">'+$prize+'</span> zł</td></tr>');
 
-// calculate cart
-function cartCalculate() {
+    // calculate carts
+    let $sum = 0;
     
+    const $prizesInTable = $('.in-table-prize');
+    for (i = 0; i < $prizesInTable.length; i++) {
+        $sum = +$sum + +$prizesInTable[i].innerText;
+    }
+    $sum = Math.round($sum*100)/100;
+    $('#sum').text('SUMA '+$sum+' zł');
 }
+
+
 
 
 
