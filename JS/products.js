4494325440fdd2f8d17acf3bc27d52e7ee95d90e
Aleksander Wiażewicz <wiazewicz@gmail.com>
2020-01-30 21:39:38 +0100

    .json try

--- JS/products.js
+++ JS/products.js
@@ -1,53 +1,30 @@
-const storage = {
-    products: [],
-    addProductToStorage: function(product) {
-        this.products.push(product);
-        const newNode = product.makeProductNode();
-        $('#all-products').append(newNode)
-    },
-    showStorage: function(){
-        console.log(this.products);
-    }
-}
-
-class Product {
-    constructor(type, imgSrc, name, brand, qty, description, prize) {
-        this.type = type,
-        this.imgSrc = imgSrc, 
-        this.name = name, 
-        this.brand = brand, 
-        this.qty = qty, 
-        this.description = description, 
-        this.prize = prize
-    }
-    makeProductNode() {
-        return $(`
-        <div class="col-6 col-sm-4 col-md-3 product ${this.type}">
-            <div class="product-card">
-                <div class="details" data-toggle="modal" data-target="#myModal3">
-                    <img src=${this.imgSrc} alt="zdjęcie produktu">
-                    <h6 class="product-name">${this.name}</h6>
-                    <p class="brand">${this.brand}</p>
-                    <div class="progress">
-                        <div class="progress-bar w-${this.qty}" role="progressbar" aria-valuenow="${this.qty}" aria-valuemin="0" aria-valuemax="100"></div>
+// get products
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var allProducts = JSON.parse(this.responseText);
+    for(i=0;i<allProducts.length;i++) {
+        const newNode = 
+            $(`
+            <div class="col-6 col-sm-4 col-md-3 product ${allProducts[i].type}">
+                <div class="product-card">
+                    <div class="details" data-toggle="modal" data-target="#myModal3">
+                        <img src=${allProducts[i].imgSrc} alt="zdjęcie produktu">
+                        <h6 class="product-name">${allProducts[i].name}</h6>
+                        <p class="brand">${allProducts[i].brand}</p>
+                        <div class="progress">
+                            <div class="progress-bar w-${allProducts[i].qty}" role="progressbar" aria-valuenow="${allProducts[i].qty}" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p class="description">${allProducts[i].description}</p>
+                        <h5><span class="product-prize">${allProducts[i].prize}</span> zł</h5>
                     </div>
-                    <p class="description">${this.description}</p>
-                    <h5><span class="product-prize">${this.prize}</span> zł</h5>
+                    <button class="add-button" data-toggle="modal" data-target="#myModal1">Dodaj do koszyka</button>
                 </div>
-                <button class="add-button" data-toggle="modal" data-target="#myModal1">Dodaj do koszyka</button>
             </div>
-        </div>
-        `)
+            `);
+            $('#all-products').append(newNode)
+        }
     }
-
-}
-
-// products
-const Product1 = new Product('gadgets promo', './images/shield.jpg', 'Szkło hartowane', 'Hard Shield', 25, 'Szkło hartowane dolor sit amet consectetur adipisicing elit. Sunt soluta atque corporis, tempora iste veritatis nam. Dicta id quam suscipit vel. Atque sed reiciendis quidem!', 9.99);
-const Product2 = new Product('toys new', './images/wristwatch.jpg', 'Okulary do strzelania', 'TopGlass', 25, 'Okulary ochronne firmy TopGlass lorem bakslcn iudcbhhi3uv uiidbh usdcwbuc udbf uidbhf uhef uedo imd', 9.99);
-
-
-
-storage.addProductToStorage(Product1);
-storage.addProductToStorage(Product2);
-storage.showStorage();
+};
+xmlhttp.open("GET", "../productsBase.json", true);
+xmlhttp.send();
